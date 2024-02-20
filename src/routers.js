138bb2f401fa99f4b1c6d59bd1563906e2a96138
isabeldiana <isabel_diana@outlook.com>
2024-02-20 19:45:12 -0300

    login

--- src/routers.js
+++ src/routers.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const users = require('./controllers/usuarios');
+const users = require('./controllers/users');
 
 const routers = express()
-routers.post('/user', users.cadastrarUsuarios),
-routers.get('/user/:id', users.exibirusuarioCadastrado)
+routers.post('/user', users.registerUser),
+routers.get('/user/:id', users.displayLogin),
+routers.put('/user/:id', users.updateUser),
+routers.post('/user/:id', users.userLogin),
 module.exports = routers
